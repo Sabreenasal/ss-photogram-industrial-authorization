0be79727c0b706ee7f905ee8e5d15ffa1bf87a15
Sabreena Salomon <salomonbree@gmail.com>
2025-07-13 18:35:02 +0000

    added authorization to user's discover and feed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -66,7 +66,7 @@ class CommentsController < ApplicationController
 
     def is_an_authorized_user
      @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
-      if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
+      if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)3
         redirect_back fallback_location: root_url, alert: "Not authorized"
       end
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,31 @@ class UsersController < ApplicationController
     @users = @q.result
   end
 
+  def feed
+    if @user != current_user
+      flash[:alert] = "You're not authorized for that"
+      redirect_to root_path
+    else
+      @feed_photos = current_user.feed
+    end
+  end
+
+  def discover
+    if @user != current_user
+      flash[:alert] = "You're not authorized for that"
+      redirect_to root_path
+    else
+      @discover_photos = current_user.discover
+    end
+  end
+
   private
 
-    def set_user
-      if params[:username]
-        @user = User.find_by!(username: params.fetch(:username))
-      else
-        @user = current_user
-      end
+  def set_user
+    if params[:username]
+      @user = User.find_by!(username: params.fetch(:username))
+    else
+      @user = current_user
     end
+  end
 end
